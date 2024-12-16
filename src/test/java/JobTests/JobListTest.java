6566664efae363650791bbd2365128451a843ffd
ramuntech <nimmala.ramu@academy.com>
2024-12-16 12:09:26 +0530

    Added new test related to job column valiation

---
+++ src/test/java/JobTests/JobListTest.java
@@ -0,0 +1,18 @@
+package JobTests;
+
+import Core.model.Common;
+import Core.test.BaseTest;
+import Pages.Admin.SaveUserPage;
+import Pages.Admin.UserManagementPage;
+import Pages.DashBoardPage;
+import Pages.LoginPage;
+import org.testng.annotations.Test;
+
+public class JobListTest extends BaseTest {
+
+    @Test(testName = "Job list column title validation")
+    public void ValidateColumnTitlesInJobListTable() {
+        LoginPage loginPage = launchApplication(LoginPage.class);
+
+    }
+}
